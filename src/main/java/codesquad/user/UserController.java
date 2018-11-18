95c52d0752e7616c65bca494ef3553d31f106278
Brad Lee <brad903@naver.com>
2018-11-18 14:53:19 +0900

    html중복 미처 하지 못한 부분 반영 및 nav부분 html 일부분 매핑

---
+++ src/main/java/codesquad/user/UserController.java
@@ -0,0 +1,12 @@
+package codesquad.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+
+}
